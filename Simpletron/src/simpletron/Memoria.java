d393beabd9feb2cc38086a85e7a98ac03e99b4e7
Moises Gomez <44556626+MoisesGomez0@users.noreply.github.com>
2019-03-02 17:20:11 -0600

    Create Memoria.java
    Este objeto es la memoria del programa

---
+++ Simpletron/src/simpletron/Memoria.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpletron;
+
+/**
+ *
+ * @author moises_2
+ */
+public class Memoria
+{
+    private int[][] memoria=new int [10][10];
+
+    
+    public int[][] getMemoria()
+    {
+        return memoria;
+    }
+
+    
+    public void setMemoria(int[][] memoria)
+    {
+        this.memoria = memoria;
+    }
+}
