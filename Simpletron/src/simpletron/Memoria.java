ba04d864a48ecd194d8777313d807482d8ea1121
Moises Gomez <44556626+MoisesGomez0@users.noreply.github.com>
2019-03-03 10:41:12 -0600

    Update Memoria.java

--- Simpletron/src/simpletron/Memoria.java
+++ Simpletron/src/simpletron/Memoria.java
@@ -12,6 +12,8 @@ package simpletron;
 public class Memoria {
 
     private int[][] memoria = new int[10][10];
+    public static int temporal=0;//Esta variable almacenara datos que se carguen por la instruccion 40
+    public static int numeroi=0;//esta variable almacenara la cantidad de instrucciones que se ingresen
 
     public int[][] getMemoria()
     {
