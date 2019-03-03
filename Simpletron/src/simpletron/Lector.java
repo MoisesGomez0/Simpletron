e938ff2080593b8acfca0bb30da02e0bb175aef7
Moises Gomez <44556626+MoisesGomez0@users.noreply.github.com>
2019-03-03 00:22:11 -0600

    Update Lector.java

--- Simpletron/src/simpletron/Lector.java
+++ Simpletron/src/simpletron/Lector.java
@@ -13,35 +13,33 @@ import java.util.Scanner;
  */
 public class Lector {
 
-    public void leerIns(int[][] b)
+    Scanner leer = new Scanner(System.in);//creando objeto Scanner para leer cosas de la consola
+
+    public void leerIns(int[][] b)//este metodo lee las instrucciones del usuario, las cuales deben ser dadas al inicio de la ejecucion
     {
-        Scanner leer = new Scanner(System.in);//creando objeto Scanner para leer cosas de la consola
 
         int temp;//variable temporal donde se guardan las cosas, tabien sirve como centinela
-        
-
-        Imprimir.mostrarmsj("Bienvenido");
-        Imprimir.mostrarmsj("ingresa las instrucciones en orden, de una en una, seguida de un enter");
 
         do
         {
+
             temp = leer.nextInt();//se lee desde la consola
             if (temp != -10)//se verifica que la lectura sea distinto de el valor clave del centinela
             {
                 for (int i = 0; i < 10; i++)
                 {
-                    boolean a=false;//se declara una variable bool para verificar que el numero fue guardado y asi salir de el segundo for
+                    boolean a = false;//se declara una variable bool para verificar que el numero fue guardado y asi salir de el segundo for
                     for (int j = 0; j < 10; j++)
                     {
                         if (b[i][j] == 0)// se verifica que el espacio este vacio
                         {
                             b[i][j] = temp;//se llena el espacio
-                            a=true;//la variable se pone en true para verificar que se guardó el de que u
+                            a = true;//la variable se pone en true para verificar que se guardó el de que u
                             break;//se sale del for interior
-                            
+
                         }
                     }
-                    if(a)
+                    if (a)
                     {
                         break;//se sale del for exterior
                     }
@@ -50,5 +48,19 @@ public class Lector {
 
         } while (temp != -10);//se verifica que el centilea sea distinto
     }
-    
+
+    public int leerDato()//este metodo lee un numero //12.21 no sirve
+    {
+        int a;
+        try
+        {
+            a = leer.nextInt();
+        } catch (Exception b)
+        {
+            Imprimir.mostrarmsj("Hey, deberias de ingresar un entero");
+            return leerDato();
+        }
+
+        return a;
+    }
 }
