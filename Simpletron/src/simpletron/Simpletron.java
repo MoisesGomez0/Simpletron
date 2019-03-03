58a90ae4967b47b73663dca08fd565d81bb46924
Moises Gomez <44556626+MoisesGomez0@users.noreply.github.com>
2019-03-03 10:41:16 -0600

    Update Simpletron.java

--- Simpletron/src/simpletron/Simpletron.java
+++ Simpletron/src/simpletron/Simpletron.java
@@ -27,8 +27,19 @@ public class Simpletron
         Imprimir.mostrarMemo(b.getMemoria());
         lee.leerIns(b.getMemoria());
         
-        Imprimir.mostrarMemo(b.getMemoria());
-        lee.leerDato();
+        for(int i=0;i<Memoria.numeroi;i++)
+        {
+            Imprimir.mostrarMemo(b.getMemoria());
+            int a=Ejecutor.puntero(b.getMemoria());
+            Imprimir.mostrarmsj("Ejecutor a "+a);
+            int instruccion=comandos.instruccion(a);
+            Imprimir.mostrarmsj("la instruccion "+instruccion);
+            int posicioni=comandos.posicioni(a);
+            Imprimir.mostrarmsj("posicion i"+posicioni);
+            int posicionj=comandos.posicionj(a);
+            Imprimir.mostrarmsj("posicion j"+posicionj);
+            Ejecutor.operacion(instruccion, posicioni,posicionj, b.getMemoria());
+        }
         
     }
     
